2b1d84fecfc7384aed5b911b7303167662e4a123
dvdkt2 <156469277+dvdkt2@users.noreply.github.com>
2025-04-13 23:56:25 -0400

    Delete curl_helpers.cpp

--- curl_helpers.cpp
+++
@@ -1,7 +0,0 @@
-#include "curl_helpers.h"
-
-size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* output) {
-    size_t totalSize = size * nmemb;
-    output->append((char*)contents, totalSize);
-    return totalSize;
-}
